ede293113d38a9af06f4892c59fc124d0477ed63
Mike Meadows <mmeadow33@gmail.com>
2016-10-14 10:54:15 -0500

    added server.js and posting data to db

--- seeds/add_User.js
+++ seeds/add_User.js
@@ -5,7 +5,10 @@ module.exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('User').insert({email: 'A@A.com', password: "password", age: 25})
+        knex('User').insert({email: 'A@A.com', password: "password", age: 25}),
+        knex('User').insert({email: 'VB@B.com', password: "password", age: 2}),
+        knex('User').insert({email: 'C@C.com', password: "password", age: 100}),
+        knex('User').insert({email: 'D@D.net', password: "password", age: 26})
       ]);
     });
   };
---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const app = express()
+const bodyParser = require('body-parser');
+const config = require('./knexfile').development
+const knex = require('knex')(config)
+
+
+const port = process.env.PORT || 3000
+
+app.set("view engine", "pug")
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.get('/', (req, res) => {
+  res.render("index");
+})
+
+app.post('/', (req, res) => {
+  Promise.all([knex('User').insert(req.body)])
+  .then((data)=> {
+    console.log(data);
+    res.sendStatus(200)
+  })
+})
+
+app.listen(port, () => {
+  console.log('Now listening on port', port)
+})
